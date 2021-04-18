597782f23d298bd4040a1d5493715d7f47e1f0a2
hntaaa <zhumadilatahan@gmail.com>
2021-04-18 21:00:17 +0600

    fixing details

--- src/components/MasterHub.vue
+++ src/components/MasterHub.vue
@@ -2,7 +2,54 @@
 <div>
   <Navigation/>
   <div class="master_hub">
-    <b-table responsive  head-row-variant="primary" hover :items="items" :fields="fields"></b-table>
+    <b-table class="myTable" responsive id="myTable" sticky-header="800px" no-border-collapse head-row-variant="primary" ref="my-table" :items="items" :fields="fields">
+      <template class="info" v-slot:cell(actions)="row">
+            <b-button size="sm" @click="toggleRowDetails(row, 'status')">
+              {{ row.detailsShowing ? 'Скрыть' : 'Редактировать'}} 
+            </b-button>
+          </template>
+
+          <template v-slot:row-details="row">
+            <b-card class="information">
+              <div>
+              <b-row
+                v-for="(detail, i) in detailsMask"
+                :key="`detail-${i}`"
+                class="mb-2" 
+              >
+              Статус: {{ row.item[detail] }} 
+              
+              </b-row>
+              
+              <b-row>
+              <b-button  size="sm-3"  class="onButton"  @click="rowOn(row.item); addHistory()" >Включить</b-button>
+              <b-button  size="sm-3"  class="offButton"  @click="rowOff(row.item); addHistory()" >Отключить</b-button>
+              <b-button  size="sm-3" class="mr-2" @click="changeId(row.item); addHistory()" >Поменять</b-button>
+              <b-button  size="sm-3" class="mr-2" @click="downloadCSVData" >Загрузить Историю</b-button>
+              </b-row>
+              <b-row>
+                <div class="comment">
+
+              <b-form-textarea
+                    id="textarea"
+                
+                    placeholder="Комментировать..."
+                    size="sm-3"
+                    rows="2"
+                    max-rows="4"
+                  ></b-form-textarea>
+                  <b-button class="addComment" size="sm">Добавить комментарий</b-button>
+                </div>
+              </b-row>
+               <b-row>
+                 <h3>История:</h3>
+                 <br>
+                <p v-for="h in history" :key="h">{{h}}, <br></p>
+              </b-row>
+              </div>
+            </b-card>
+          </template>
+    </b-table>
   </div>
 </div>
 </template>
@@ -33,6 +80,19 @@ import Navigation from '@/components/Navigation.vue'
             label: 'Дом',
           },
           {
+            key: 'type_house',
+            label: 'Наименование строение',
+          },
+          
+          {
+            key: 'podezd',
+            label: 'Подъезд',
+          },
+          {
+            key: 'shahta',
+            label: 'Шахта',
+          },
+          {
             key: 'id_master_hub',
             label: 'ID МастерХаба',
           },
@@ -40,21 +100,59 @@ import Navigation from '@/components/Navigation.vue'
             key: 'status',
             label: 'Статус',
           },
+          {
+            key: 'actions',
+            label: '',
+          },
 
         ],
         items: [
-          { _rowVariant: "light",isActive: true, city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', id_master_hub: '1521544',  status: 'включен',},
-          { _rowVariant: "light",isActive: true, city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', id_master_hub: '1521544',  status: 'включен',},
-          { _rowVariant: "light",isActive: true, city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', id_master_hub: '1521544',  status: 'включен',},
-          { _rowVariant: "light",isActive: true, city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', id_master_hub: '1521544',  status: 'включен',},
-          { _rowVariant: "light",isActive: true, city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', id_master_hub: '1521544',  status: 'включен',},
-          { _rowVariant: "light",isActive: true, city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', id_master_hub: '1521544',  status: 'включен',},
-          { _rowVariant: "light",isActive: true, city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', id_master_hub: '1521544',  status: 'включен',},
-          { _rowVariant: "light",isActive: true, city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', id_master_hub: '1521544',  status: 'включен'}
+          { _rowVariant: "light",isActive: true, type_house: "ЖК", city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', podezd: "2", shahta: "первая", id_master_hub: '1521544',  status: 'включен',},
+          { _rowVariant: "light",isActive: true, type_house: "ЖК", city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', podezd: "2", shahta: "первая", id_master_hub: '1521544',  status: 'включен',},
+          { _rowVariant: "light",isActive: true, type_house: "ЖК", city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', podezd: "2", shahta: "первая", id_master_hub: '1521544',  status: 'включен',},
+          { _rowVariant: "light",isActive: true, type_house: "ЖК", city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', podezd: "2", shahta: "первая", id_master_hub: '1521544',  status: 'включен',},
+          { _rowVariant: "light",isActive: true, type_house: "ЖК", city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', podezd: "2", shahta: "первая", id_master_hub: '1521544',  status: 'включен',},
+          { _rowVariant: "light",isActive: true, type_house: "ЖК", city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', podezd: "2", shahta: "первая", id_master_hub: '1521544',  status: 'включен',},
+          { _rowVariant: "light",isActive: true, type_house: "ЖК", city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', podezd: "2", shahta: "первая", id_master_hub: '1521544',  status: 'включен',},
+          { _rowVariant: "light",isActive: true, type_house: "ЖК", city: "Алматы", raion: 'Есиль', street: 'Кабанбай Батыр', house: '15', podezd: "2", shahta: "первая", id_master_hub: '1521544',  status: 'включен'}
       
         ]
   }
-  }
+  },
+  methods: {
+    toggleRowDetails(row, data) {
+      this.detailsMask = data.split(', ')
+      row.toggleDetails()
+    },
+    rowOn(item) {
+      this.$set(item, "status", "включен")
+    },
+    rowOff(item) {
+      this.$set(item, "status", "отключен")
+    },
+    changeId(item){
+      this.$set(item, "id", Math.floor(Math.random()*1000000))
+    },
+    turnOnAllHere(data){
+      this.count = data;
+    }, 
+    
+    addHistory(){
+      this.history.push("Hello")
+    },
+    downloadCSVData() {
+    let csv = 'Put,Column,Titles,Here\n';
+    this.history.forEach((row) => {
+            csv += row.join(',');
+            csv += "\n";
+    });
+ 
+    const anchor = document.createElement('a');
+    anchor.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv);
+    anchor.target = '_blank';
+    anchor.download = 'история.csv';
+    anchor.click();
+}}
   }
 </script>
 
@@ -63,7 +161,42 @@ import Navigation from '@/components/Navigation.vue'
   
   .master_hub{
     margin-top: 1rem;
+  }
+  .card{
+    margin-right: 1rem;
+    border: none;
+  }
+
+  .onButton{
+    margin-right: 1rem;
+    background-color: #629e6c;
+  }
+  .offButton{
+    margin-right: 1rem;
+    background-color: #f57575;
+  }
+  
+  .comment{
+    display: flex;
+    margin-top: 2rem;
+  }
+  .addComment{
+    margin-left: 1rem;
+  }
+  .information > div{
+    margin-left: 1rem;
+  }
+
+  p{
+    margin-top: 1.5rem;
+  }
+  h3{
+    margin-top: 1rem;
+  }
+  
+  .myTable{
+    border-radius: 1rem;
     border-style: solid;
-    border-width: medium;
+    border-width: thin;
   }
 </style>
